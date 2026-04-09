5bc3a562d628baefde7ba494af44028be3542af5
jeronimoaguirre11 <jeronimoaguirre1671@gmail.com>
2026-04-09 12:29:47 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './pages/home/home';
+import { Portfolio } from './pages/portfolio/portfolio';
+import { Services } from './pages/services/services';
+import { Contact } from './pages/contact/contact';
+
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'portafolio', component: Portfolio },
+  { path: 'servicios', component: Services },
+  { path: 'contacto', component: Contact },
+];
