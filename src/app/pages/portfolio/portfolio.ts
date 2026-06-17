45692b55842534e8bb9ccaa4fba92c5c5cb5b49b
jeronimoaguirre11 <jeronimoaguirre1671@gmail.com>
2026-06-17 17:04:26 -0500

    actualizacion del proyecto costaseal

--- src/app/pages/portfolio/portfolio.ts
+++ src/app/pages/portfolio/portfolio.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
@@ -6,6 +6,24 @@ import { Component } from '@angular/core';
   templateUrl: './portfolio.html',
   styleUrl: './portfolio.css',
 })
-export class Portfolio {
 
+export class Portfolio implements OnInit {
+
+  images: string[] = [
+    '/img1.jpg',
+    '/img2.jpg',
+    '/logo.jpg'
+  ];
+
+  currentIndex = 0;
+
+  ngOnInit() {
+    setInterval(() => {
+      this.nextSlide();
+    }, 3000); // cambia cada 3 segundos
+  }
+
+  nextSlide() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
 }
--- src/app/pages/services/services.ts
+++ src/app/pages/services/services.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-services',
@@ -6,6 +6,23 @@ import { Component } from '@angular/core';
   templateUrl: './services.html',
   styleUrl: './services.css',
 })
-export class Services {
+export class Services implements OnInit {
 
+  images: string[] = [
+    '/img1.jpg',
+    '/img2.jpg',
+    '/logo.jpg'
+  ];
+
+  currentIndex = 0;
+
+  ngOnInit() {
+    setInterval(() => {
+      this.nextSlide();
+    }, 3000); // cambia cada 3 segundos
+  }
+
+  nextSlide() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
 }
