bbb40ca90a91b447e438c0fd2bc122da2a382bb0
jeronimoaguirre11 <jeronimoaguirre1671@gmail.com>
2026-06-19 12:47:45 -0500

    pagina responsive completado

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -9,9 +9,20 @@ import { RouterModule } from '@angular/router';
 })
 export class Header {
   isScrolled = false;
+  isMenuOpen = false;
 
   @HostListener('window:scroll')
   onScroll() {
     this.isScrolled = window.scrollY > 40;
+  }
+
+  // agregado para el menú lateral en móvil
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
   }
 }
